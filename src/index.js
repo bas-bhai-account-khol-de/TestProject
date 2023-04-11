c452c50587aee8ff2d02c3e8296da04a0d477e3c
Prakhar <89300995+bas-bhai-account-khol-de@users.noreply.github.com>
2023-04-11 23:14:00 +0530

    force back camera

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function(stream) {
-        video.srcObject = stream;
+const constraints = {
+    video: {
+
+      facingMode: "environment"
+    }
+  };
+  const video =document.getElementById('video');
+
+async function getDevices() {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log(devices);
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+        const updatedConstraints = {
+          ...constraints,
+        //   deviceId: {
+        //     exact: videoDevices[0]
+        //   }
+        };
+        const stream = await navigator.mediaDevices.getUserMedia(updatedConstraints);
+        video.srcObject = stream
         video.play();
-    })
-    .catch(function(err) {
-        console.log("An error occurred! " + err);
-    });
+      }
+
+
+  }
+
+
+
+
+    function opencvIsReady(){
+        getDevices();
+        document.getElementById('status').innerHTML="loaded ";
+    }
