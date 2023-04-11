b0a2b51ea4af830bdd4b66cf2f01ec4a022a81b6
Prakhar <89300995+bas-bhai-account-khol-de@users.noreply.github.com>
2023-04-12 00:40:58 +0530

    orb fratures

--- src/index.js
+++ src/index.js
@@ -26,10 +26,72 @@ async function getDevices() {
 
   }
 
-
-
-
-    function opencvIsReady(){
-        getDevices();
-        document.getElementById('status').innerHTML="loaded ";
+var Module = {
+    onRuntimeInitialized() {
+        document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+        opencvIsReady();
     }
+}
+
+
+
+
+var orb=null
+let cap = null;
+let frame =null;
+    function opencvIsReady(){
+        document.getElementById('status').innerHTML="loaded ";
+        getDevices();
+        cap=new cv.VideoCapture(video);
+        orb = new cv.ORB(200);
+        frame = new cv.Mat(240,320,cv.CV_8UC4);
+        trackimage();
+    }
+
+
+/// image tracking code
+var desc_target = null;
+var kp_target=null;
+var desc_video = null;
+var kp_video=null;
+
+//tempp
+
+
+function trackimage(){
+
+    //requirmnents
+    
+    getImageFeatures();
+    desc_video= new cv.Mat();
+    kp_video = new cv.KeyPointVector();
+    getVideoFeatures();
+    
+    
+}
+
+function getVideoFeatures(){
+    cap.read(frame);
+    orb.detectAndCompute(frame,new cv.Mat(),kp_video,desc_video)
+    cv.drawKeypoints(frame,kp_video,frame,[0, 255, 0, 255])
+    cv.imshow('canvasOutput',frame);
+    requestAnimationFrame(getVideoFeatures);
+    
+}
+
+function getImageFeatures(){
+    const image = new Image();
+    image.src = './src/target.jpeg'
+    image.onload =()=>{
+        let mat = cv.imread(image);
+        cv.cvtColor(mat,mat,6)
+        console.log(cv);
+         
+         desc_target = new cv.Mat();
+         kp_target = new cv.KeyPointVector();
+         orb.detectAndCompute(mat,new cv.Mat(),kp_target,desc_target)
+         cv.drawKeypoints(mat,kp_target,mat,[0, 255, 0, 255])
+        //  cv.imshow('canvasOutput',mat,10,10)
+    }
+     
+}
